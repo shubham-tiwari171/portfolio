65c445f7849a489c6a99e5e393d846ca1e482b62
Shubham <tshubhamucem@gmail.com>
2023-07-10 10:59:56 +0530

    css changed

--- portfolio/src/components/molecule/Home/Home.jsx
+++ portfolio/src/components/molecule/Home/Home.jsx
@@ -4,10 +4,10 @@ import "./Home.css"; // Assuming you have a separate CSS file for styling
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="overlay"></div> {/* Add an overlay element */}
+      <div className="overlay"></div>
       <div className="left-section">
         <h1 className="animated-text continuous-animation">SHUBHAM TIWARI</h1>
-        <p className="animated-text continuous-animation  ">
+        <p className="animated-text continuous-animation">
           FRONTEND WEB-DEVELOPER
         </p>
       </div>
